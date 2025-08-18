38f2c13af8a6a06624ce1483b2137d4e888fd6b4
Yagame <Gustavo_guaresi@estudante.sesisenai.org.br>
2025-08-18 19:53:59 -0300

    Add files via upload

--- Programação de Aplicativos/Aula/construtor/PrincipaL1.java
+++ Programação de Aplicativos/Aula/construtor/PrincipaL1.java
@@ -2,7 +2,6 @@ package Aula.construtor;
 
 public class PrincipaL1 {
     public static void main(String[] args) {
-        animal somAnimal = new Animal();
-        somAnimal.fazerSom();
+        
     }
 }
